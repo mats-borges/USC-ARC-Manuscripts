2d2887b5c13293ceaf4b73e54a0b949642a0f8bc
Mats <35862270+mats-borges@users.noreply.github.com>
2022-01-28 23:11:04 -0800

    Update MovementManager.cs

--- Assets/Scripts/MovementManager.cs
+++ Assets/Scripts/MovementManager.cs
@@ -8,8 +8,16 @@ using OVR;
 public class MovementManager : MonoBehaviour
 {
     [SerializeField] private GameObject player;
+    [SerializeField] private GameObject LController, RController;
+
+    [SerializeField] private GameObject TrackingSpace;
+    [SerializeField] private GameObject CenterEyeAnchor;
+    
     private Rigidbody pr;
     private Transform pt;
+    
+    private Quaternion lookDirection = Quaternion.identity;
+    private Vector3 normalizedLookDirection = Vector3.forward;
     public float playerSpeed = 1;
 
     private void Start()
@@ -21,11 +29,22 @@ public class MovementManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        var centerEyeTransform = CenterEyeAnchor.transform;
+
+        lookDirection = centerEyeTransform.transform.rotation;
+        lookDirection.eulerAngles = new Vector3(0, lookDirection.eulerAngles.y, 0);
+        
+        normalizedLookDirection = lookDirection * Vector3.forward;
+        var lookDirectionRight = Vector3.Cross(Vector3.up, normalizedLookDirection);
+
         var joystickAxisR = OVRInput.Get(OVRInput.RawAxis2D.LThumbstick, OVRInput.Controller.LTouch);
         var joystickAxisU = OVRInput.Get(OVRInput.RawAxis2D.RThumbstick, OVRInput.Controller.RTouch);
+        
+        var fwdMove = joystickAxisR.y * Time.deltaTime * playerSpeed * (normalizedLookDirection);
+        var strafeMove = joystickAxisR.x * Time.deltaTime * playerSpeed * (lookDirectionRight);
+        pr.position += fwdMove + strafeMove;
 
-        pr.position += (transform.right * joystickAxisR.x + transform.forward * joystickAxisR.y) * Time.deltaTime * playerSpeed;
-        pr.position += (transform.right * joystickAxisU.x + transform.up * joystickAxisU.y) * Time.deltaTime * playerSpeed;
+        pr.position += joystickAxisU.y * Time.deltaTime * playerSpeed * transform.up;
 
     }
 }
